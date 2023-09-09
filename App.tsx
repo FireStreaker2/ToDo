de27d900b93e8e1a3739acc53de5c40e247f9b26
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-09 12:33:11 -0700

    navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,44 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+interface HomeScreenProps {
+	navigation: any;
+}
+
+const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.h1}>ToDo</Text>
 			<Text style={styles.p}>Welcome Back!</Text>
+			<Button title="Go To App" onPress={() => navigation.navigate("App")} />
 			<StatusBar style="auto" />
 		</View>
 	);
-}
+};
+
+const AppScreen: React.FC = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Hello</Text>
+		</View>
+	);
+};
+
+const App: React.FC = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="App" component={AppScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
@@ -28,3 +56,5 @@ const styles = StyleSheet.create({
 		fontSize: 10,
 	},
 });
+
+export default App;
