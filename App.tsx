44bb56961c1602c3a1ee7cac19103125d5f7871a
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-10 18:39:22 -0700

    todo creation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 const Stack = createStackNavigator();
 
-interface HomeScreenProps {
+let todos = [
+	{
+		title: "Create a new TODO",
+		description: 'Press the "Create New TODO" Button!',
+	},
+];
+
+interface NavigationProps {
 	navigation: any;
 }
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+const HomeScreen: React.FC<NavigationProps> = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.h1}>ToDo</Text>
@@ -21,10 +28,71 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
 	);
 };
 
-const AppScreen: React.FC = () => {
+const AppScreen: React.FC<NavigationProps> = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
-			<Text>Hello</Text>
+			<>
+				{todos.map((item, index) => (
+					<Text key={index}>
+						{item.title}, {item.description}
+					</Text>
+				))}
+			</>
+			<Button
+				title="Create New TODO"
+				onPress={() => navigation.navigate("CreateToDo")}
+			/>
+		</View>
+	);
+};
+
+const CreateToDoScreen: React.FC<NavigationProps> = ({ navigation }) => {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+
+	const handleTitleChange = (text: string) => {
+		setTitle(text);
+	};
+
+	const handleDescriptionChange = (text: string) => {
+		setDescription(text);
+	};
+
+	const submitData = () => {
+		if (!title && !description) {
+			alert("Please Enter A Value.");
+			return;
+		}
+
+		todos.push({
+			title: title,
+			description: description,
+		});
+
+		alert("Succesfully Submitted!");
+		navigation.navigate("Home");
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>Enter Info</Text>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Title"
+					onChangeText={handleTitleChange}
+					onSubmitEditing={submitData}
+					value={title}
+					style={styles.input}
+				/>
+				<TextInput
+					placeholder="Description"
+					onChangeText={handleDescriptionChange}
+					onSubmitEditing={submitData}
+					value={description}
+					style={styles.input}
+				/>
+			</View>
+			<Button title="Submit" onPress={submitData} />
 		</View>
 	);
 };
@@ -35,6 +103,7 @@ const App: React.FC = () => {
 			<Stack.Navigator>
 				<Stack.Screen name="Home" component={HomeScreen} />
 				<Stack.Screen name="App" component={AppScreen} />
+				<Stack.Screen name="CreateToDo" component={CreateToDoScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
@@ -54,6 +123,16 @@ const styles = StyleSheet.create({
 
 	p: {
 		fontSize: 10,
+	},
+
+	input: {
+		backgroundColor: "#cacaca",
+		textAlign: "center",
+		margin: 3,
+	},
+
+	inputContainer: {
+		margin: 30,
 	},
 });
 
